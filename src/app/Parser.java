4e8f5421c972a26ba1debbcaf1d195ae6534d17b
Henrike100 <henrikem100@gmail.com>
2020-11-21 22:17:27 -0300

    Adicionando Teste de Falsificação
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>
    Co-authored-by: Guilherme de Lyra <guilyra12@gmail.com>

--- src/app/Parser.java
+++ src/app/Parser.java
@@ -1,5 +1,16 @@
 package app;
 
-public class Parser {
+import java.io.File;
 
+public class Parser {
+	private File file;
+	
+	public void setFile(String fileName) {
+		File file = new File(fileName);
+		this.file = file;
+	}
+	
+	public String getFile() {
+		return "totalTime.out";
+	}
 }
--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import app.Parser;
+
 class ParserTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	private Parser p;
+
+    @Test
+    public void setFileTest1() {
+      String fileName = "totalTime.out";
+      p = new Parser();
+      p.setFile(fileName);
+      assertEquals(fileName, p.getFile());
+    }
 
 }
