f9ee6f8f119fad10c222bde079349bae9c340946
Guilherme de Lyra <guilyra12@gmail.com>
2020-11-22 16:48:46 -0300

    Adicionando testes parametrizados

--- src/tst/HelpersTest.java
+++ src/tst/HelpersTest.java
@@ -9,9 +9,24 @@ import utils.Helpers;
 public class HelpersTest {
 
 	@Test
-	void arrayContainsCharTest() {
+	void arrayContainsCharTest1() {
 		char[] charArray = new char[] { 'a' };
 		char searchedChar = 'a';
 		assertEquals(true, Helpers.arrayContainsChar(searchedChar, charArray));
+	}
+	
+	@Test
+	void arrayContainsCharTest2() {
+		char[] charArray = new char[] { 'b' };
+		char searchedChar = 'a';
+		assertEquals(false, Helpers.arrayContainsChar(searchedChar, charArray));
+	}
+	
+	@Test
+	void arrayContainsCharTest3() {
+		char[] charArray = new char[] { 'a', 'b' };
+		assertEquals(true, Helpers.arrayContainsChar('a', charArray));
+		assertEquals(true, Helpers.arrayContainsChar('b', charArray));
+		assertEquals(false, Helpers.arrayContainsChar('c', charArray));
 	}
 }
--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -16,6 +16,10 @@ import org.junit.jupiter.params.provider.ValueSource;
 import app.Parser;
 import exceptions.EscritaNaoPermitidaException;
 import exceptions.DelimitadorInvalidoException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 class ParserTest {
 		private Parser p;
@@ -110,4 +114,50 @@ class ParserTest {
 		Assertions.assertThrows(DelimitadorInvalidoException.class, () -> p.setDelimiter("abc"));
 		Assertions.assertThrows(DelimitadorInvalidoException.class, () -> p.setDelimiter(""));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+    		"a", "b", "c", "d", "e",
+    		"f", "g", "h", "i", "j",
+    		"k", "l", "m", "n", "o",
+    		"p", "q", "r", "s", "t",
+    		"u", "v", "w", "x", "y",
+    		"z", "!", "@", "#", "$",
+    		"%", "&", "*", "(", ")",
+    		"-", "_", "=", "+", "`",
+    		"[", "{", "}", "]", "~",
+    		"^", ".", ",", "<", ">",
+    		";", ":", "/", 
+    		"\t", "\b", "\n", "\r",
+    		"\f", "\'", "\"", "\\",
+    		"\\t", "\\b", "\\n", "\\r",
+    		"\\f", "\\\'", "\\\"", "\\\\"
+    	})
+    void setDelimiterParameterizedTestValidDelimiters(String delimiter) throws DelimitadorInvalidoException {
+    	p = new Parser();
+    	p.setDelimiter(delimiter);
+    	char delimiterAsChar = delimiter.charAt(0);
+		assertEquals(delimiterAsChar, p.getDelimiter());
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {
+    		"", "ab", "abc", "\nabc",
+    		"\nn", "\\nn", "\\c", "\\d",
+    		"\\e", "\\g", "\\h", "\\i",
+    		"\\j", "\\k", "\\l", "\\m",
+    		"\\o", "\\p", "\\q", "\\s",
+    		"\\u", "\\v", "\\x", "\\w",
+    		"\\y", "\\z", "\\@", "\\!",
+    		"\\#", "\\$", "\\%", "\\&",
+    		"\\*", "\\(", "\\)", "\\-",
+    		"\\=", "\\[", "\\]", "\\{",
+    		"\\}", "\\`", "\\,", "\\.",
+    		"\\;", "\\ "
+    })
+    void setDelimiterParameterizedTestInvalidDelimiters(String delimiter) throws DelimitadorInvalidoException {
+    	p = new Parser();
+    	Assertions.assertThrows(DelimitadorInvalidoException.class, () -> p.setDelimiter(delimiter));
+    }
+    
 }
