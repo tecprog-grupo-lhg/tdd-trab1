b4a5462c9296484a64fc0d557f125a10459cfaf2
Henrike100 <henrikem100@gmail.com>
2020-11-21 22:03:57 -0300

    Estrutura Inicial
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>
    Co-authored-by: Guilherme de Lyra <guilyra12@gmail.com>

---
+++ src/tst/ParserTest.java
@@ -0,0 +1,14 @@
+package tst;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ParserTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
