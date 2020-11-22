fdbbe5af74388a85c15038b21371dad9749c269e
Guilherme de Lyra <guilyra12@gmail.com>
2020-11-22 17:35:59 -0300

    adicionando testes iniciais

--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -5,10 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ParserTest {
-
 	@Test
-	void test() {
+	void testSave() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testSaveAsColumn() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	void testSaveAsRow() {
+		fail("Not yet implemented");
+	}
 }
