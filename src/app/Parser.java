cae4f6788ae0a5fa2bb63c9f3da7486d582f8fbe
Henrike100 <henrikem100@gmail.com>
2020-11-21 22:37:36 -0300

    Adicionando Teste de Duplicação e Exceções
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>
    Co-authored-by: Guilherme de Lyra <guilyra12@gmail.com>

--- src/app/Parser.java
+++ src/app/Parser.java
@@ -2,15 +2,24 @@ package app;
 
 import java.io.File;
 
+import exceptions.ArquivoNaoEncontradoException;
+
 public class Parser {
 	private File file;
 	
-	public void setFile(String fileName) {
+	public void setFile(String fileName) throws ArquivoNaoEncontradoException{
 		File file = new File(fileName);
+		checkIfFileExists(file);
 		this.file = file;
 	}
 	
 	public String getFile() {
 		return "totalTime.out";
+	}
+	
+	private void checkIfFileExists(File file) throws ArquivoNaoEncontradoException {
+		if(!file.exists()) {
+			throw new ArquivoNaoEncontradoException(file.getName());
+		}
 	}
 }
--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -1,21 +1,27 @@
 package tst;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import app.Parser;
+import exceptions.ArquivoNaoEncontradoException;
 
 class ParserTest {
 
 	private Parser p;
 
     @Test
-    public void setFileTest1() {
+    public void setFileTest1() throws ArquivoNaoEncontradoException {
       String fileName = "totalTime.out";
       p = new Parser();
       p.setFile(fileName);
       assertEquals(fileName, p.getFile());
     }
-
+    
+    @Test
+    public void setFileTest2() throws ArquivoNaoEncontradoException {
+      p = new Parser();
+      Assertions.assertThrows(ArquivoNaoEncontradoException.class, () -> p.setFile("thisfiledoesnotexistsforsure.txt"));
+    }
 }
