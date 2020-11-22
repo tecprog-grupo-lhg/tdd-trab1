e2a2d67644382dafe60b7faf2e38b0e5b1d9a5b2
Guilherme de Lyra <guilyra12@gmail.com>
2020-11-22 19:00:24 -0300

    adicionando teste de falsificação para metodo setFormat

--- src/app/Parser.java
+++ src/app/Parser.java
@@ -1,5 +1,13 @@
 package app;
 
 public class Parser {
-
+	private String formatToSave;
+	
+	public String getFormatToSave() {
+		return this.formatToSave;
+	}
+	
+	public void setFormatToSave(String format) {
+		this.formatToSave = format;
+	}
 }
--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -4,7 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import app.Parser;
 class ParserTest {
+	private Parser p;
+	
+	@Test
+	void testSetFormat() {
+		p = new Parser();
+		p.setFormatToSave("column");
+		assertEquals("column", p.getFormatToSave());
+	}
+	
 	@Test
 	void testSave() {
 		fail("Not yet implemented");
