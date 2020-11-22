56d004adf99be83c9e490883dc8863f863f1f740
Guilherme de Lyra <guilyra12@gmail.com>
2020-11-22 13:45:18 -0300

    Adicionando primeiro teste para Delimitador

--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -18,43 +18,43 @@ import app.Parser;
 import exceptions.EscritaNaoPermitidaException;
 
 class ParserTest {
-	private Parser p;
+		private Parser p;
 	
-	@Test
-	void testWritOutputFile() throws EscritaNaoPermitidaException {
-	      p = new Parser();
-	      p.setFile("analysisTime.out");
-	      
-	      
-	      p.setOutPut( new File(System.getProperty("user.dir")).toPath().toString());	      
-	      Path actual = p.getPath();
-	      
-	      Path path =  new File(System.getProperty("user.dir") + "/analysisTimeTab.out").toPath();
-	      
-	      assertEquals(path, actual);
-	}
+		@Test
+		void testWritOutputFile() throws EscritaNaoPermitidaException {
+					p = new Parser();
+					p.setFile("analysisTime.out");
+					
+					
+					p.setOutPut( new File(System.getProperty("user.dir")).toPath().toString());	      
+					Path actual = p.getPath();
+					
+					Path path =  new File(System.getProperty("user.dir") + "/analysisTimeTab.out").toPath();
+					
+					assertEquals(path, actual);
+		}
 	
-	@Test
-	void testWritOutputFile2() throws EscritaNaoPermitidaException {
-	      p = new Parser();
-	      p.setFile("analysisTime.out");
-	      
-	      Assertions.assertThrows(EscritaNaoPermitidaException.class, () -> p.setOutPut("~/root"));
-	}
+		@Test
+		void testWritOutputFile2() throws EscritaNaoPermitidaException {
+					p = new Parser();
+					p.setFile("analysisTime.out");
+					
+					Assertions.assertThrows(EscritaNaoPermitidaException.class, () -> p.setOutPut("~/root"));
+		}
 
-	@Test
-	void testWritOutputFile3() throws EscritaNaoPermitidaException {
-	      p = new Parser();
-	      p.setFile("analysisTime.out");
-	      
-	      p.setOutPut( new File(System.getProperty("user.dir")).toPath().toString());	      
-	      Path actual = p.getPath();
-	      
-	      Path path =  new File(System.getProperty("user.dir") + "/analysisTimeTab.out").toPath();
-	      
-	      assertEquals(path, actual);
-	      Assertions.assertThrows(EscritaNaoPermitidaException.class, () -> p.setOutPut("~/root"));
-	}
+		@Test
+		void testWritOutputFile3() throws EscritaNaoPermitidaException {
+					p = new Parser();
+					p.setFile("analysisTime.out");
+					
+					p.setOutPut( new File(System.getProperty("user.dir")).toPath().toString());	      
+					Path actual = p.getPath();
+					
+					Path path =  new File(System.getProperty("user.dir") + "/analysisTimeTab.out").toPath();
+					
+					assertEquals(path, actual);
+					Assertions.assertThrows(EscritaNaoPermitidaException.class, () -> p.setOutPut("~/root"));
+		}
 	
     @ParameterizedTest
     @CsvSource( {
@@ -84,4 +84,8 @@ class ParserTest {
     	Assertions.assertThrows(EscritaNaoPermitidaException.class, () -> p.setOutPut("~/root"));
     }
 	
+		@Test
+		void testDelimiter() {
+			fail("Not yet implemented");
+		}
 }
