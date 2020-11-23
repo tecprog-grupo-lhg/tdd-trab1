297f28970cd577fa46875e49fa47f2d104ef96a7
Henrike100 <henrikem100@gmail.com>
2020-11-23 00:23:29 -0300

    Testes parametrizados

--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -5,13 +5,79 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import app.Parser;
 
+@RunWith(Parameterized.class)
 class ParserTest {
-	Parser p;
+	private Object[][] parameters = new Object[][]{
+		{
+			"analysis.out",
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("333", "807", "123")),
+				new ArrayList<String>(Arrays.asList("333", "807")),
+				new ArrayList<String>(Arrays.asList("333"))
+			)
+		},
+		{
+			"analysis2.out",
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("243", "645", "123", "567")),
+				new ArrayList<String>(Arrays.asList("333", "807", "756", "234", "345")),
+				new ArrayList<String>(Arrays.asList("333", "756", "123"))
+			)
+		},
+		{
+			"analysis3.out",
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("333", "807", "123")),
+				new ArrayList<String>(Arrays.asList("333", "807")),
+				new ArrayList<String>(Arrays.asList("333")),
+				new ArrayList<String>(Arrays.asList("333", "807", "123", "432", "867", "122", "98")),
+				new ArrayList<String>(Arrays.asList()),
+				new ArrayList<String>(Arrays.asList("333", "54"))
+			)
+		},
+		{
+			"analysis4.out",
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("12345"))
+			)
+		},
+		{
+			"analysis5.out",
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123")),
+				new ArrayList<String>(Arrays.asList("123"))
+			)
+		},
+	};
+	private Parser p;
 
 	@Test
     public void parseTest1() {
@@ -79,5 +145,18 @@ class ParserTest {
 		);
 
 		assertEquals(expected, p.getTable());
+    }
+	
+	@ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4})
+    void parseParameterizedTest(int index) {
+		p = new Parser();
+		File file = new File((String)parameters[index][0]);
+		p.setFile(file);
+		p.parse();
+		
+		Object obj = parameters[index][1];
+
+		assertEquals(obj, p.getTable());
     }
 }
