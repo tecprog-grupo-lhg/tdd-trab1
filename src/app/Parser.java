b5d3b78534bd46fe0020057bb5dcc3c83f894f2b
Henrike100 <henrikem100@gmail.com>
2020-11-22 22:29:17 -0300

    Teste de Falsificação

--- src/app/Parser.java
+++ src/app/Parser.java
@@ -1,5 +1,28 @@
 package app;
 
-public class Parser {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Parser {
+	private File file;
+	private ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
+	
+	public void setFile(File file) {
+		this.file = file;
+	}
+	
+	public void parse() {
+		table = new ArrayList<ArrayList<String>>(
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("333", "807", "123")),
+				new ArrayList<String>(Arrays.asList("333", "807")),
+				new ArrayList<String>(Arrays.asList("333"))
+			)
+		);
+	}
+	
+	public ArrayList<ArrayList<String>> getTable() {
+		return this.table;
+	}
 }
--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -2,13 +2,33 @@ package tst;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
+import app.Parser;
+
 class ParserTest {
+	Parser p;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void parseTest1() {
+		p = new Parser();
+		File file = new File("analysis.out");
+		p.setFile(file);
+		p.parse();
+		
+		ArrayList<ArrayList<String>> expected = new ArrayList<ArrayList<String>>(
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("333", "807", "123")),
+				new ArrayList<String>(Arrays.asList("333", "807")),
+				new ArrayList<String>(Arrays.asList("333"))
+			)
+		);
+
+		assertEquals(expected, p.getTable());
+    }
 
 }
