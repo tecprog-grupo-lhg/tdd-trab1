84e84f9ee9ff3f035fdad2a1f8dd70ec6481ccf6
Guilherme de Lyra <guilyra12@gmail.com>
2020-11-24 00:32:40 -0300

    Adicionando nova Exceção (FormatoDeSaídaInvalidoException) e adequando as funçoes/testes para utiliza-la;
    Adicionando, também, testes parametrizados.

--- src/app/Parser.java
+++ src/app/Parser.java
@@ -1,13 +1,38 @@
 package app;
 
+import exceptions.FormatoDeSaídaInvalidoException;
+
 public class Parser {
 	private String formatToSave;
-	
+
 	public String getFormatToSave() {
 		return this.formatToSave;
 	}
 	
-	public void setFormatToSave(String format) {
+	public void setFormatToSave(String format) throws FormatoDeSaídaInvalidoException {
+		checkValidOutputFormat(format);
 		this.formatToSave = format;
+	}
+	
+	private void checkValidOutputFormat(String format) throws FormatoDeSaídaInvalidoException {
+		if (!format.equals("column") && !format.equals("row")) {
+			throw new FormatoDeSaídaInvalidoException(format);
+		}
+	}
+	
+	public void save() {
+		if (this.formatToSave.equals("column")) {
+			saveAsColumn();
+		} else {
+			saveAsRow();
+		}
+	}
+	
+	public void saveAsRow() {
+		
+	}
+
+	public void saveAsColumn() {
+
 	}
 }
--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -2,28 +2,32 @@ package tst;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import app.Parser;
+import exceptions.FormatoDeSaídaInvalidoException;
+
 class ParserTest {
 	private Parser p;
 	
 	@Test
-	void testSetFormat1() {
+	void testSetFormat1() throws FormatoDeSaídaInvalidoException {
 		p = new Parser();
 		p.setFormatToSave("column");
 		assertEquals("column", p.getFormatToSave());
 	}
 
 	@Test
-	void testSetFormat2() {
+	void testSetFormat2() throws FormatoDeSaídaInvalidoException {
 		p = new Parser();
-		p.setFormatToSave("row");
-		assertNotEquals("column", p.getFormatToSave());
+		Assertions.assertThrows(FormatoDeSaídaInvalidoException.class, () -> p.setFormatToSave("formato aleatorio"));
 	}
-	
+    
 	@Test
-	void testSetFormat3() {
+	void testSetFormat3() throws FormatoDeSaídaInvalidoException {
 		p = new Parser();
 		p.setFormatToSave("column");
 		assertEquals("column", p.getFormatToSave());
@@ -36,11 +40,28 @@ class ParserTest {
 		
 		p.setFormatToSave("row");
 		assertNotEquals("column", p.getFormatToSave());
+	
+		Assertions.assertThrows(FormatoDeSaídaInvalidoException.class, () -> p.setFormatToSave("outro formato"));
 	}
 	
+    @ParameterizedTest
+    @ValueSource(strings = { "row", "column" })
+    void setFormatParameterizedWithAllowedFormats(String format) throws FormatoDeSaídaInvalidoException {
+    	p = new Parser();
+		p.setFormatToSave(format);
+    	assertEquals(format, p.getFormatToSave());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "", "a", "abc", "outro formato", "formato invalido", "diagonal" })
+    void setFormatParameterizedWithInvalidFormats(String format) throws FormatoDeSaídaInvalidoException {
+    	p = new Parser();
+		Assertions.assertThrows(FormatoDeSaídaInvalidoException.class, () -> p.setFormatToSave(format));
+    }
+    
 	@Test
 	void testSave() {
-		fail("Not yet implemented");
+		fail("Not yet implemented");	
 	}
 	
 	@Test
