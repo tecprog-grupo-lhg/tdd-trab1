97380b416e0005d9784af8f19840190d8a4754f5
Henrike100 <henrikem100@gmail.com>
2020-11-23 22:15:36 -0300

    Adicionando Teste de Falsificação

--- src/app/Parser.java
+++ src/app/Parser.java
@@ -1,5 +1,65 @@
 package app;
 
-public class Parser {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
+public class Parser {
+	private Path output;
+	private String format;
+	private ArrayList<ArrayList<String>> table;
+	private char delimiter;
+	
+	public void setOutput(String output) {
+		Path path = new File(output).toPath();
+		this.output = path;
+	}
+	
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+	public void setTable(ArrayList<ArrayList<String>> table) {
+		this.table = table;
+	}
+	
+	public void setDelimiter(char delimiter) {
+		this.delimiter = delimiter;
+	}
+	
+	public void save() {
+		if(format.equals("colunas")) {
+			saveWithColumnFormat();
+		}
+		else {
+			saveWithRowFormat();
+		}
+	}
+	
+	private void saveWithColumnFormat() {
+		try {
+			FileWriter myWriter = new FileWriter(output.toString());
+			myWriter.write("1;2;3\n");
+			myWriter.write("333;333;333\n");
+			myWriter.write("807;807\n");
+			myWriter.write("123\n");
+		    myWriter.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+	
+	private void saveWithRowFormat() {
+		try {
+			FileWriter myWriter = new FileWriter(output.toString());
+			myWriter.write("1;333;807;123\n");
+			myWriter.write("2;333;807\n");
+			myWriter.write("3;333\n");
+		    myWriter.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
 }
--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -2,13 +2,102 @@ package tst;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
 
+import app.Parser;
+
 class ParserTest {
+	private Parser p;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void saveRowTest1() throws IOException {
+		p = new Parser();
+		File output = new File("analysisTimeTab.out");
+		p.setOutput(output.getAbsolutePath());
+		p.setFormat("linhas");
+		
+		ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>(
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("333", "807", "123")),
+				new ArrayList<String>(Arrays.asList("333", "807")),
+				new ArrayList<String>(Arrays.asList("333"))
+			)
+		);
+		
+		p.setTable(table);
+		
+		p.setDelimiter(';');
+		p.save();
+		
+		ArrayList<String> expected = new ArrayList<String>(Arrays.asList(
+				"1;333;807;123",
+				"2;333;807",
+				"3;333"));
 
+		ArrayList<String> linhas = new ArrayList<String>();
+		
+		try {
+			Scanner sc = new Scanner(output);
+			while (sc.hasNextLine()) {
+		        final String linha = sc.nextLine();
+		        linhas.add(linha);
+		    }
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		output.delete();
+		assertEquals(expected, linhas);
+    }
+	
+	@Test
+    public void saveColumnTest1() throws IOException {
+		p = new Parser();
+		File output = new File("analysisTimeTab.out");
+		p.setOutput(output.getAbsolutePath());
+		p.setFormat("colunas");
+		
+		ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>(
+			Arrays.asList(
+				new ArrayList<String>(Arrays.asList("333", "807", "123")),
+				new ArrayList<String>(Arrays.asList("333", "807")),
+				new ArrayList<String>(Arrays.asList("333"))
+			)
+		);
+		
+		p.setTable(table);
+		
+		p.setDelimiter(';');
+		p.save();
+		
+		ArrayList<String> expected = new ArrayList<String>(Arrays.asList(
+				"1;2;3",
+				"333;333;333",
+				"807;807",
+				"123"));
+
+		ArrayList<String> linhas = new ArrayList<String>();
+		
+		try {
+			Scanner sc = new Scanner(output);
+			while (sc.hasNextLine()) {
+		        final String linha = sc.nextLine();
+		        linhas.add(linha);
+		    }
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		output.delete();
+		assertEquals(expected, linhas);
+    }
 }
