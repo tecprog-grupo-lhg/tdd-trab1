9837e2b335b52cb90e73cc1f22451aa6acbf6c23
LuisCL94 <lclaudio.tl@gmail.com>
2020-11-24 20:29:05 -0300

    Adicionando teste de falsificacao
    Co-authored-by: Henrike100 <henrikem100@gmail.com>

--- src/app/Parser.java
+++ src/app/Parser.java
@@ -1,5 +1,33 @@
 package app;
 
-public class Parser {
+import java.io.File;
+import java.nio.file.Path;
 
+import exceptions.EscritaNaoPermitidaException;
+
+public class Parser {
+	
+	private File file;
+	private Path output;
+	
+	public void setFile(String fileName) {
+		File file = new File(fileName);
+		this.file = file;
+	}
+	
+	public void setOutPut(String output) {
+		Path path = new File(output).toPath();
+		
+		if(file.getName().equals("analysisTime.out")) {
+			this.output = new File(output + "/analysisTimeTab.out").toPath();
+		}
+		else {
+			this.output = new File(output + "/totalTimeTab.out").toPath();
+		}
+	}	
+
+	public Path getPath() {
+		return this.output;
+	}
 }
+
--- src/tst/ParserTest.java
+++ src/tst/ParserTest.java
@@ -1,14 +1,33 @@
 package tst;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.nio.file.Path;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-class ParserTest {
+import app.Parser;
 
+class ParserTest {
+	private Parser p;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testWritOutputFile() {
+	      p = new Parser();
+	      p.setFile("analysisTime.out");
+	      
+	      
+	      p.setOutPut( new File(System.getProperty("user.dir")).toPath().toString());	      
+	      Path actual = p.getPath();
+	      
+	      Path path =  new File(System.getProperty("user.dir") + "/analysisTimeTab.out").toPath();
+	      
+	      assertEquals(path, actual);
 	}
 
+	
 }
